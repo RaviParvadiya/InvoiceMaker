81ccd178a16681abf08dfa969e630973465cc483
RaviParvadiya <raviparvadiya.2001@gmail.com>
2024-02-23 13:49:18 +0530

    first commit

--- client/src/api/baseUrl.js
+++ client/src/api/baseUrl.js
@@ -1,6 +1,7 @@
 import axios from 'axios'
 
-const API = axios.create({ baseURL: 'http://localhost:5000'})
+// const API = axios.create({ baseURL: 'http://localhost:5000'})
+const API = axios.create({ baseURL: 'https://invoice-server-sp5o.onrender.com'})
 // const API = axios.create({ baseURL: process.env.REACT_APP_API})
 
 export const fetchInvoices =() => API.get('/invoices/getallinvoices')
